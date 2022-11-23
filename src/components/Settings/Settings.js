df4d52f01546057d3ce16ddd8544a4391607dead
Emilio <toschi17@yahoo.i>
2022-11-23 11:27:22 +0100

    Creato SETTINGS e SUBMENU

--- src/components/Settings/Settings.js
+++ src/components/Settings/Settings.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+
+import classes from './Settings.module.css';
+
+import SubMenu from './SubMenu';
 
 function Settings() {
-	return <div>Settings</div>;
+	return (
+		<div className={classes.wrapper}>
+			<p className={classes.header}>Impostazioni</p>
+			<SubMenu />
+			<Outlet />
+		</div>
+	);
 }
 
 export default Settings;
---
+++ src/components/Settings/SubMenu.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import { NavLink } from 'react-router-dom';
+
+import classes from './SubMenu.module.css';
+
+import { MenuElements } from './../../__data/common';
+import { hScroll } from '../../lib/horizontalScrol';
+
+function SubMenu() {
+	const parentRoute = 'Impostazioni';
+
+	const [menuData, setMenuData] = useState(
+		MenuElements.filter(el => el.description === parentRoute)
+	);
+
+	useEffect(() => {
+		hScroll('MENU');
+	}, []);
+
+	const getSubMenuElements = () => {
+		const elements = menuData[0].subMenu;
+		const visualData = elements.map(e => {
+			console.log(parentRoute);
+			return (
+				<NavLink
+					key={e._id}
+					className={navData =>
+						`${classes.navEl} ${navData.isActive && classes.active}`
+					}
+					to={`/${parentRoute}${e.path}`}
+				>
+					{e.description}
+				</NavLink>
+			);
+		});
+		return visualData;
+	};
+
+	return (
+		<div id='MENU' className={classes.wrapper}>
+			{getSubMenuElements()}
+		</div>
+	);
+}
+
+export default SubMenu;
